bae3f1891847435424ebd773cc7fa09eaa3d7a03
ashuforweb <ashuforweb@gmail.com>
2016-08-15 02:48:44 +0800

    Push any()

--- built-in.py
+++ built-in.py
@@ -16,11 +16,20 @@ print abs(x) #This will print 1.7267467272723723823
 x = complex(1+3j) #This will print magnitude of 1=3j which is calculated as √(1^2+3^2)
 print abs(x)
 
-'''This is about all() function. The single argument is to be iterable and
-the output is True if all the elements of iterables are true or iterable is empty. '''
+'''This is about all(iterable) function. The single argument is to be iterable and
+the output is True if all the elements of iterables are true or iterable is empty else False.'''
 theList = []
 print all(theList) # This will print True as the iterable list 'theList' is empty
 theList = [True,True,True,True,True,True,True,]
 print all(theList) # This will print True as all the elements of this list are True.
 theList = [True,True,True,False]
 print all(theList) # This will print False as not all the elements (last element is False) are not true.
+
+'''This is about any(iterable) function. The single argument is to be iterable and
+the output is True if any of the elements of iterables are True else False.'''
+theList = [False,False,True,False,False]
+print any(theList) # This will print True as 3rd element of the list is true.
+theList = []
+print any(theList) # This will print False as it's emplty list.
+theList = [False,False,False,False]
+print any(theList) # This will print False as all elements are False.
