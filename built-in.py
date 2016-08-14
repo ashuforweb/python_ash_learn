19bf8648da433c837a8f0b0651373f415f40287e
ashuforweb <ashuforweb@gmail.com>
2016-08-15 02:40:41 +0800

    Push all()

--- built-in.py
+++ built-in.py
@@ -4,7 +4,7 @@ This file is to be used for reference and explain the functions in more detail v
 
 
 '''The first built-in function is abs(x): Return the absolute value of a number.
- The argument may be a plain or long integer or a floating point number.
+ The single argument may be a plain or long integer or a floating point number.
  If the argument is a complex number, its magnitude is returned.'''
 # To explain and examplified this let's initiate x as some number
 x = 1
@@ -13,5 +13,14 @@ x = 1.5
 print abs(x) #This will print 1.5
 x = 1.7267467272723723823
 print abs(x) #This will print 1.7267467272723723823
-x = complex(1+3j) #This will return magnitude of 1=3j which is calculated as √(1^2+3^2)
+x = complex(1+3j) #This will print magnitude of 1=3j which is calculated as √(1^2+3^2)
 print abs(x)
+
+'''This is about all() function. The single argument is to be iterable and
+the output is True if all the elements of iterables are true or iterable is empty. '''
+theList = []
+print all(theList) # This will print True as the iterable list 'theList' is empty
+theList = [True,True,True,True,True,True,True,]
+print all(theList) # This will print True as all the elements of this list are True.
+theList = [True,True,True,False]
+print all(theList) # This will print False as not all the elements (last element is False) are not true.
