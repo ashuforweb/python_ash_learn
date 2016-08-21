66352fc203c790c8f71584fd46756d5ce4846f8b
ashuforweb <ashuforweb@gmail.com>
2016-08-21 22:26:39 +0800

    List changes and examples

---
+++ python_list.py
@@ -0,0 +1,7 @@
+#Python lists and methods
+'''append(x) this method takes element as an argument and add the element into
+end of the list. For Example if you need to add 8 in existing list listTest'''
+listTest = []
+print listTest #will retrun empty list
+listTest.append(8)
+print listTest # will return list with element 8
