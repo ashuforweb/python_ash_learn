12ca136f43b0116957767f77d17cdc16855271d3
ashuforweb <ashuforweb@gmail.com>
2016-08-21 23:22:08 +0800

    list insert push

--- python_list.py
+++ python_list.py
@@ -9,3 +9,10 @@ print listTest # will return list with element 8
 '''extend(L) takes List as an argument and return joined list'''
 listTest.extend(listTest)
 print listTest #will print two elements 8,8
+
+'''insert(i,x) takes indices and element as arguments, and inserts element at
+given indices and pushes other elements down. For example if element is add at
+location 2 of length 2 list 2nd element will now become 3rd element. indices
+starts at 0'''
+listTest.insert(1,5)
+print listTest
